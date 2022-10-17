abbd5f777d6a0ddd964bd72d3849be7a5f938e4b
Joe Appleton <joe.appleton@solent.ac.uk>
2022-10-17 14:49:21 +0100

    changed the data file

--- src/mocks/data.js
+++ src/mocks/data.js
@@ -34,51 +34,47 @@ export const roomTypes = [
 
 export const rooms = [
   {
-    id: 1,
     number: "208",
     building: "HC",
     capacity: 10,
-    type: {code: "pc", name: "PC Lab"},
+    type: { code: "pc", name: "PC Lab" },
   },
   {
-    id: 2,
     number: "204",
     building: "HC",
     capacity: 25,
-    type: {code: "pc", name: "Mac Lab"},
+    type: { code: "pc", name: "Mac Lab" },
   },
   {
-    id: 3,
+
     number: "203",
     building: "HC",
     capacity: 15,
-    type: {code: "pc", name: "PC Lab"},
+    type: { code: "pc", name: "PC Lab" },
   },
   {
-    id: 4,
+
     number: "206",
     building: "HC",
     capacity: 25,
-    type: {code: "pc", name: "Mac Lab"},
+    type: { code: "pc", name: "Mac Lab" },
   },
   {
-    id: 5,
     number: "202",
     building: "HC",
     capacity: 13,
-    type: {code: "pc", name: "PC Lab"},
+    type: { code: "pc", name: "PC Lab" },
   },
   {
-    id: 6,
     number: "201",
     building: "HC",
     capacity: 25,
-    type: {code: "pc", name: "Mac Lab"},
+    type: { code: "pc", name: "Mac Lab" },
   },
 ];
 
 export const lotsOfRooms = [
   ...rooms,
-  ...rooms.map((r) => ({...r, ...{building: "RM"}})),
-  ...rooms.map((r) => ({...r, ...{building: "JM"}})),
+  ...rooms.map((r) => ({ ...r, ...{ building: "RM" } })),
+  ...rooms.map((r) => ({ ...r, ...{ building: "JM" } })),
 ];
