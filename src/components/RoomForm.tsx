67fab6fe16ab0a86e1f37bc6868021fd1305281f
Joe Appleton <joe.appleton@solent.ac.uk>
2022-10-24 13:20:31 +0100

    updated room form

--- src/components/RoomForm.tsx
+++ src/components/RoomForm.tsx
@@ -1,17 +1,24 @@
 import {CloudUploadIcon} from "@heroicons/react/outline";
+import {buildings} from "../mocks/data";
 
 export default function RoomForm() {
   return (
-    <>
+    <form>
       <div className="flex flex-col align-middle  space-y-2">
-        <h3 className="text-lg text-center mb-2"> Add A Room</h3>
         <a className="gray-outline-button">
           <CloudUploadIcon className="h-5 w-5" /> Add Room Photos
         </a>
         <label className="font-semibold"> Building</label>
-        <select className="border-2 rounded-md p-2 text-gray-400">
-          <option> Building </option>
-        </select>
+        <>
+          {buildings.map((b, i) => (
+            <div key={i} className="flex space-x-2">
+              <input type="checkbox" value={b.code} name="building"></input>
+              <label className="text-sm">
+                {b.name} ({b.code})
+              </label>
+            </div>
+          ))}
+        </>
 
         <label className="font-semibold"> Room Number</label>
         <input
@@ -26,11 +33,12 @@ export default function RoomForm() {
           placeholder="Capacity"
         />
       </div>
+
       <div className="flex justify-center w-full mt-3">
         <div>
           <button className="blue-button_no-icon ">Add Room</button>
         </div>
       </div>
-    </>
+    </form>
   );
 }
--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -6,6 +6,7 @@ export default function Create() {
     <div className="max-w-[80%] mx-auto mt-11">
       <BreadCrumb label={"Add Room"} />
       <div className="bg-white mx-auto mt-11 p-5 rounded-lg shadow-md">
+        <h3 className="text-lg text-center mb-2"> Add A Room</h3>
         <RoomForm />
       </div>
     </div>
